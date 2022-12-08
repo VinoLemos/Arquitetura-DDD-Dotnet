ea0d4e94f410f9c8c6719c276ff3d252b674fc42
Vinicius Lemos de Oliveira <vlemosdeoliveira@gmail.com>
2022-12-08 15:32:34 -0300

    Swagger adicionado e configurado

--- src/Api.Application/Startup.cs
+++ src/Api.Application/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace application
 {
@@ -31,6 +32,26 @@ namespace application
             ConfigureRepository.ConfigureDependenciesRepository(services);
 
             services.AddControllers();
+            services.AddSwaggerGen( c => 
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Curso de API com AspNetCore 3.1 - Na Prática",
+                    Description = "Arquitetura DDD",
+                    TermsOfService = new Uri("http://www.mrinfo.com.br"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Vinícius Lemos de Oliveira",
+                        Email = "vlemosdeoliveira@gmail.com",
+                        Url = new Uri("https://linkedin.com/in/vinolemos")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Termo de Licensa de Uso",
+                        Url = new Uri("https://www.github.com/vinolemos")
+                }});
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +62,14 @@ namespace application
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json",
+                                "Curso de API com AspNetCore 3.1");
+                c.RoutePrefix = string.Empty;
+            }
+            );
             app.UseRouting();
 
             app.UseAuthorization();
