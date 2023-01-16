c966d6d496d439dd51a089b81f2eb9f88258d1a1
Vinicius Lemos de Oliveira <vlemosdeoliveira@gmail.com>
2023-01-16 17:02:39 -0300

    Mappings de endereco

---
+++ src/Api.Data/Mapping/CepMap.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Api.Data.Mapping
+{
+    public class CepMap : IEntityTypeConfiguration<CepEntity>
+    {
+        public void Configure(EntityTypeBuilder<CepEntity> builder)
+        {
+            builder.ToTable("Cep");
+
+            builder.HasKey(c => c.Id);
+
+            builder.HasIndex(c => c.Cep);
+
+            builder.HasOne(c => c.Municipio)
+                   .WithMany(m => m.Ceps);
+        }
+    }
+}
