99bf503b4779bb286fe8aca9fe3995fd833d3ad9
Vinicius Lemos de Oliveira <vlemosdeoliveira@gmail.com>
2023-02-03 15:23:18 -0300

    Mapeamento de endereco

--- src/Api.CrossCutting/Mappings/DtoToModelProfile.cs
+++ src/Api.CrossCutting/Mappings/DtoToModelProfile.cs
@@ -1,3 +1,6 @@
+using Api.Domain.Dtos.Cep;
+using Api.Domain.Dtos.Municipio;
+using Api.Domain.Dtos.Uf;
 using Api.Domain.Dtos.User;
 using Api.Domain.Models;
 using AutoMapper;
@@ -8,16 +11,43 @@ namespace Api.CrossCutting.Mappings
     {
         public DtoToModelProfile()
         {
-            // < source, destination >
+            #region User
             CreateMap<UserModel, UserDto>()
-                    // converts from destination to source
                     .ReverseMap();
+
             CreateMap<UserModel, UserDtoCreate>()
-
                     .ReverseMap();
+
             CreateMap<UserModel, UserDtoUpdate>()
-
                     .ReverseMap();
+            #endregion
+
+            #region Uf
+            CreateMap<UFModel, UfDto>()
+                    .ReverseMap();
+            #endregion
+
+            #region Municipio
+            CreateMap<MunicipioModel, MunicipioDto>()
+                    .ReverseMap();
+
+            CreateMap<MunicipioModel, MunicipioDtoCreate>()
+                    .ReverseMap();
+
+            CreateMap<MunicipioModel, MunicipioDtoUpdate>()
+                    .ReverseMap();
+            #endregion
+
+            #region Cep
+            CreateMap<CepModel, CepDto>()
+                    .ReverseMap();
+
+            CreateMap<CepModel, CepDtoCreate>()
+                    .ReverseMap();
+
+            CreateMap<CepModel, CepDtoUpdate>()
+                    .ReverseMap();
+            #endregion
         }
     }
 }
--- src/Api.CrossCutting/Mappings/EntityToDtoProfile.cs
+++ src/Api.CrossCutting/Mappings/EntityToDtoProfile.cs
@@ -1,3 +1,6 @@
+using Api.Domain.Dtos.Cep;
+using Api.Domain.Dtos.Municipio;
+using Api.Domain.Dtos.Uf;
 using Api.Domain.Dtos.User;
 using Api.Domain.Entities;
 using AutoMapper;
@@ -8,14 +11,39 @@ namespace Api.CrossCutting.Mappings
     {
         public EntityToDtoProfile()
         {
+            #region User
             CreateMap<UserDto, UserEntity>()
                         .ReverseMap();
-
             CreateMap<UserDtoCreateResult, UserEntity>()
                         .ReverseMap();
-
             CreateMap<UserDtoUpdateResult, UserEntity>()
                         .ReverseMap();
+            #endregion
+
+            #region Uf
+            CreateMap<UfDto, UfEntity>()
+                        .ReverseMap();
+            #endregion
+
+            #region Municipio
+            CreateMap<MunicipioDto, MunicipioEntity>()
+                        .ReverseMap();
+            CreateMap<MunicipioDtoCompleto, MunicipioEntity>()
+                        .ReverseMap();
+            CreateMap<MunicipioDtoCreateResult, MunicipioEntity>()
+                        .ReverseMap();
+            CreateMap<MunicipioDtoUpdateResult, MunicipioEntity>()
+                        .ReverseMap();
+            #endregion
+
+            #region Cep
+            CreateMap<CepDto, CepEntity>()
+                        .ReverseMap();
+            CreateMap<CepDtoCreateResult, CepEntity>()
+                        .ReverseMap();
+            CreateMap<CepDtoUpdateResult, CepEntity>()
+                        .ReverseMap();
+            #endregion
         }
     }
 }
